5e5401e84dde54e67981bb2fb845cd61f602f1a5
zheka <zheka@HOME-PC>
2025-09-13 17:42:57 +0300

    pagenation DONE

--- src/components/Search.js
+++ src/components/Search.js
@@ -1,37 +1,57 @@
 import React from 'react';
 import './Search.css';
 
-class Search extends React.Component
-{
- state =
-  {
-    search:""
+class Search extends React.Component {
+  state = {
+    search: ""
   }
 
-  handleKey = (event) =>
-  {
-   if(event.key === 'Enter')this.props.searchMovie(this.state.search, this.state.type);
+  handleKey = (event) => {
+    if(event.key === 'Enter') {
+      this.props.searchMovie(this.state.search, this.props.searchType);
+    }
+  }
+
+  // Обработчик изменения типа контента
+  handleTypeChange = (event) => {
+    this.props.onTypeChange(event.target.value);
   }
  
- render()
- {
-  return(
-     <>
-         <div className ='search'>
-            <input
-               type="search"
-               placeholder = 'Введите название фильма'
-               value={this.state.search}
-               onChange={(e)=> this.setState({search: e.target.value})}
-               onKeyDown={this.handleKey}
-           />
-           {/* <button className= 'btn' onClick={this.props.searchMovie(this.state.search,this.state.type)}>
-             Поиск
-           </button> */}
-
-         </div>
-     </>
+  render() {
+    return(
+      <>
+        <div className='search'>
+          <input
+            type="search"
+            placeholder='Введите название фильма, сериала или игры'
+            value={this.state.search}
+            onChange={(e) => this.setState({search: e.target.value})}
+            onKeyDown={this.handleKey}
+            className="search-input"
+          />
+          
+          {/* Выпадающий список для выбора типа контента */}
+          <select 
+            value={this.props.searchType} 
+            onChange={this.handleTypeChange}
+            className="type-select"
+          >
+            <option value="all">All</option>
+            <option value="movie">Films</option>
+            <option value="series">Series</option>
+            <option value="game">Games</option>
+          </select>
+          
+          <button 
+            className='search-btn' 
+            onClick={() => this.props.searchMovie(this.state.search, this.props.searchType)}
+          >
+            Поиск
+          </button>
+        </div>
+      </>
     )
- }
+  }
 }
+
 export default Search;
--- src/layout/Main.js
+++ src/layout/Main.js
@@ -4,49 +4,145 @@ import MovieList from '../components/MovieList.js';
 import Search from '../components/Search.js';
 import './Main.css';
 
-class Main extends React.Component
-{
- state = {movies: [], loading: false} // Добавили состояние loading
+class Main extends React.Component {
+  state = {
+    movies: [],
+    loading: false,
+    currentPage: 1,
+    moviesPerPage: 4,
+    searchType: "all" // Добавляем тип поиска по умолчанию
+  }
 
- componentDidMount()
- {
-  fetch('https://omdbapi.com/?apikey=fa5c825f&s=terminator')
-  .then(response=>response.json())
-  .then(data=>this.setState({movies:data.Search}))
- }
- 
- searchMovie = (str)=>
- {
-  this.setState({loading: true}) // Устанавливаем флаг загрузки
-  fetch(`https://omdbapi.com/?apikey=fa5c825f&s=${str.trim()}`)
-  .then(response=>response.json())
-  .then(data=>{
-    // Явно проверяем успешность ответа и очищаем старые данные при ошибке
-    if (data.Response === "True") {
-      this.setState({movies: data.Search, loading: false})
-    } else {
-      this.setState({movies: [], loading: false}) // Очищаем при ошибке
+  componentDidMount() {
+    // Добавляем тип в начальный запрос
+    fetch('https://omdbapi.com/?apikey=fa5c825f&s=TERMINATOR&type=movie')
+      .then(response => response.json())
+      .then(data => this.setState({ movies: data.Search }))
+  }
+
+  // Обновляем функцию поиска с учетом типа
+  searchMovie = (str, type = this.state.searchType) => {
+    this.setState({ loading: true });
+    
+    // Формируем URL с учетом выбранного типа
+    let url = `https://omdbapi.com/?apikey=fa5c825f&s=${encodeURIComponent(str.trim())}`;
+    if (type !== "all") {
+      url += `&type=${type}`;
     }
-  })
- 
- }
- 
- render()
- {
-  return(
-     <div className='main'>
-        <div className='wrap'> {/* Исправлено: classname -> className */}
-          <Search searchMovie={this.searchMovie}/>
-           {
-            // Добавляем проверку на loading и пустой список
-            this.state.loading ? <Preloader/> : 
-            this.state.movies.length ? <MovieList movies={this.state.movies}/> : 
-            <div>Ничего не найдено</div>
-           }     
-         </div>
+    
+    fetch(url)
+      .then(response => response.json())
+      .then(data => {
+        if (data.Response === "True") {
+          this.setState({ 
+            movies: data.Search, 
+            loading: false,
+            currentPage: 1,
+            searchType: type // Сохраняем выбранный тип
+          });
+        } else {
+          this.setState({ movies: [], loading: false });
+        }
+      })
+      .catch(error => {
+        this.setState({ movies: [], loading: false });
+      })
+  }
+
+  // Функция для изменения типа контента
+  handleTypeChange = (newType) => {
+    this.setState({ searchType: newType }, () => {
+      // Если есть текущий поисковый запрос, выполняем новый поиск с выбранным типом
+      if (this.state.movies.length > 0) {
+        this.searchMovie(this.state.searchTerm || "TERMINATOR", newType);
+      }
+    });
+  }
+
+  nextPage = () => {
+    this.setState(prevState => ({
+      currentPage: prevState.currentPage + 1
+    }));
+  }
+
+  prevPage = () => {
+    this.setState(prevState => ({
+      currentPage: Math.max(prevState.currentPage - 1, 1)
+    }));
+  }
+
+  goToPage = (pageNumber) => {
+    this.setState({
+      currentPage: pageNumber
+    });
+  }
+
+  render() {
+    const { movies, loading, currentPage, moviesPerPage, searchType } = this.state;
+    
+    const startIndex = (currentPage - 1) * moviesPerPage;
+    const endIndex = startIndex + moviesPerPage;
+    const currentMovies = movies.slice(startIndex, endIndex);
+    
+    const totalPages = Math.ceil(movies.length / moviesPerPage);
+    const pageNumbers = [];
+    for (let i = 1; i <= totalPages; i++) {
+      pageNumbers.push(i);
+    }
+
+    return(
+      <div className='main'>
+        <div className='wrap'>
+          {/* Передаем в Search функцию для изменения типа и текущий тип */}
+          <Search 
+            searchMovie={this.searchMovie} 
+            searchType={searchType}
+            onTypeChange={this.handleTypeChange}
+          />
+          
+          {loading && <Preloader/>}
+          
+          {!loading && currentMovies.length > 0 && <MovieList movies={currentMovies}/>}
+          
+          {!loading && movies.length === 0 && <div>Ничего не найдено</div>}
+          
+          {!loading && movies.length > moviesPerPage && (
+            <div className="pagination">
+              <button 
+                onClick={this.prevPage} 
+                disabled={currentPage === 1}
+                className="pagination-btn pagination-prev"
+              >
+                Back
+              </button>
+              
+              <div className="page-numbers">
+                {pageNumbers.map(number => (
+                  <button
+                    key={number}
+                    onClick={() => this.goToPage(number)}
+                    className={`page-number-btn ${currentPage === number ? 'active' : ''}`}
+                  >
+                    {number}
+                  </button>
+                ))}
+              </div>
+              
+              <button 
+                onClick={this.nextPage}
+
+
+disabled={currentPage === totalPages}
+                className="pagination-btn pagination-next"
+              >
+                Prev
+              </button>
+            </div>
+          )}
+        </div>
       </div>
     )
- }
+  }
 }
 
 export default Main;
